e6450d7ff08b7ab6e38a51784408cdc984e74d50
Justice Payne <spikepayne@yahoo.com>
2020-08-27 23:25:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 array = ["kiwk"]
 def oxford_comma(array)
-  array = ["kiwi"].join
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-end 
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{{array[-1]}}"
+    return array.join(", ")
+  end
+end
